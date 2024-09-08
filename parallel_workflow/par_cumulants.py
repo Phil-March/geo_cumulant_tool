a1dd40cadb5f20ff67726c7c5db009d4f0c976be
Phil-March <philippe.marchand2@mail.mcgill.ca>
2024-09-08 13:17:37 -0400

    removed fixed values

--- parallel_workflow/par_cumulants.py
+++ parallel_workflow/par_cumulants.py
@@ -35,7 +35,7 @@ def associate_grade(df_data, pairs_file):
     return df_pairs
 
 
-def compute_3rd_order_cumulant(df_pairs, num_chunks=4):
+def compute_3rd_order_cumulant(df_pairs, num_chunks):
     def process_chunk(chunk, df_pairs):
         # Merge for direction 0
         result = chunk.merge(
@@ -116,7 +116,7 @@ def compute_3rd_order_cumulant(df_pairs, num_chunks=4):
     return final_result
 
 
-def compute_4th_order_cumulant(df_pairs, num_chunks=10):
+def compute_4th_order_cumulant(df_pairs, num_chunks):
     def process_chunk(chunk, df_pairs):
         # Merge for direction 0
         result = chunk.merge(
