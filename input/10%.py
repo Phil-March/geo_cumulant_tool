c3613d116a53036b8c06c774df55db35e35f9471
Phil-March <philippe.marchand2@mail.mcgill.ca>
2024-10-19 22:10:28 -0400

    new dataset

--- input/10%.py
+++ input/10%.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv('input/walker_lake.csv')
 
 # Filter rows where 'Y' and 'X' are between 0 and 50
-df_filtered = df[(df['Y'] >= 0) & (df['Y'] <= 50) & (df['X'] >= 0) & (df['X'] <= 50)]
+df_filtered = df[(df['Y'] >= 0) & (df['Y'] <= 50) & (df['X'] >= 0) & (df['X'] <= 50) & (df['Z'] >= 0) & (df['Z'] <= 50)]
 
 # Randomly sample 50% of the remaining rows
-df_sampled = df_filtered.sample(frac=0.50, random_state=42)
+df_sampled = df_filtered.sample(frac=0.30, random_state=42)
 
 # Save the reduced dataset to a new CSV file
 df_sampled.to_csv('input/3d_walker_lake.csv', index=False)
